dda2d0f62f2eee6e5f205eb67dc161c1f7a4d8b3
Nathan Williams <nate88williams@gmail.com>
2024-01-28 17:27:15 -0800

    Update TailwindCSS configuration file
    Included '@nextui-org/react' in the Tailwind configuration to enhance UI features. Also added 'vivid-sky-blue' and 'light-soft-blue' as new color options to the theme. Furthermore, the 'darkMode' option has been set to 'class'.

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,21 @@
+import {nextui} from "@nextui-org/react";
+
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
+    "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}"
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'vivid-sky-blue': '#00ABE4',
+        'light-soft-blue': '#E9F1FA'
+      },
+    },
   },
-  plugins: [],
+  darkMode: "class",
+  plugins: [nextui()],
 }
 
