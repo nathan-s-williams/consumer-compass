2a88b3f43d031822a42ed1feccee7a72d1d3e1fa
Nathan Williams <nate88williams@gmail.com>
2024-03-01 21:30:07 -0600

    Add Footer component to App
    The App component has been updated to include the Footer component. This new structure ensures that Footer is visible on the screen across the app, thereby enhancing consistency and user experience.

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 // import './App.css'
 import NavBar from "./Components/NavBar/NavBar.tsx";
+import Footer from "./Components/Footer/Footer.tsx";
 
 function App() {
   return (
-    <>
+    <div className="min-h-screen">
         <NavBar />
-    </>
+        <Footer />
+    </div>
   )
 }
 
