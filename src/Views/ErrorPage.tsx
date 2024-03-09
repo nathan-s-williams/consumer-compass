aa7859efcbcb3427530916958bf8a682050d7a8b
Nathan Williams <nate88williams@gmail.com>
2024-03-08 19:20:16 -0600

    Add ErrorPage component for handling routing errors
    A new ErrorPage.tsx file has been added which creates a React component. This component uses the useRouteError hook from react-router-dom to display user-friendly error details when a route doesn't exist or fails for other reasons. An interface for RouterError was also defined to present proper errors.

---
+++ src/Views/ErrorPage.tsx
@@ -0,0 +1,24 @@
+import { useRouteError } from "react-router-dom";
+
+interface RouterError extends Error {
+    statusText: string;
+}
+function ErrorPage() {
+    // @ts-ignore
+    const error: RouterError = useRouteError();
+
+    return (
+        <div className="min-h-screen flex justify-center items-center flex-col gap-12">
+            <div className="flex items-end gap-6">
+                <span className="text-8xl">404</span>
+                <span className="pb-1.5 text-2xl">Page Not Found!</span>
+            </div>
+            <div className="flex flex-col items-center gap-6">
+                <p>Details:</p>
+                <p className="text-xl"><i>{error.statusText || error.message || "Cannot find page"}</i></p>
+            </div>
+        </div>
+    );
+}
+
+export default ErrorPage;
