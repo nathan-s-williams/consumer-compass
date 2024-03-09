16c794e80164d239a5db99c11b8e4897cd4890cd
Nathan Williams <nate88williams@gmail.com>
2024-03-08 19:27:43 -0600

    Add TypeScript ESLint rules and plugin to .eslintrc
    The ESLint configuration file (.eslintrc.cjs) has been updated with the addition of TypeScript ESLint recommended rules and plugin. In addition, the TypeScript ESLint parser has been specified. The change is necessary to ensure that our TypeScript code is consistently formatted and follows best TypeScript practices.

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -3,18 +3,27 @@ module.exports = {
   env: { browser: true, es2020: true },
   extends: [
     'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
     'plugin:react/recommended',
     'plugin:react/jsx-runtime',
     'plugin:react-hooks/recommended',
   ],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
   parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
+  parser: '@typescript-eslint/parser',
   settings: { react: { version: '18.2' } },
-  plugins: ['react-refresh'],
+  plugins: [
+      'react-refresh',
+      '@typescript-eslint'
+  ],
   rules: {
     'react-refresh/only-export-components': [
       'warn',
       { allowConstantExport: true },
     ],
+    // '@typescript-eslint/interface-name-prefix': [
+    //   'warn',
+    //   { 'prefixWithI': 'always' }
+    // ],
   },
 }
