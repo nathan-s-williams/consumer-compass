c15a088dbb8a40feb45c91f98b003a7eb4690b3a
Nathan Williams <nate88williams@gmail.com>
2024-03-08 19:26:35 -0600

    Refactor Footer.tsx for default export
    The Footer component exportation in Footer.tsx has been transformed from a named export to a default export. This change improves consistency in the way components are exported, making the import process simpler and more coherent across the application.

--- src/Components/Footer/Footer.tsx
+++ src/Components/Footer/Footer.tsx
@@ -1,7 +1,9 @@
-export function Footer() {
+function Footer() {
     return(
         <div className="fixed bottom-0 inset-x-0 h-20 bg-gray-200 flex items-center">
             <p className="ml-5 text-black">Copyright { new Date().getFullYear() }</p>
         </div>
     );
 }
+
+export default Footer;
