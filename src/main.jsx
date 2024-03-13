9108ae92249b72895974f95a423d822ad8a3f810
Nathan Williams <nate88williams@gmail.com>
2024-03-12 20:42:58 -0500

    Implement routing and add new view components
    Added routing configuration to main.jsx using react-router-dom. Also introduced new view components: Dashboard, Login, and ErrorPage. The routing logic is set to serve appropriate views based on the path users navigate to.

--- src/main.jsx
+++ src/main.jsx
@@ -2,13 +2,30 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import {NextUIProvider} from '@nextui-org/react'
+import { createBrowserRouter, RouterProvider } from "react-router-dom"
 import App from './App'
 import './index.css'
+import ErrorPage from "./Views/ErrorPage.tsx";
+import Login from "./Views/Login.tsx";
+import Dashboard from "./Views/Dashboard.tsx";
+
+const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <App />,
+        errorElement: <ErrorPage />,
+        children: [
+            { index: true, element: <Dashboard /> },
+            { path: "login", element: <Login /> },
+            { path: "dashboard", element: <Dashboard /> }
+        ]
+    },
+]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     <React.StrictMode>
         <NextUIProvider>
-            <App />
+            <RouterProvider router={router} />
         </NextUIProvider>
-    </React.StrictMode>,
-)
+    </React.StrictMode>
+);
