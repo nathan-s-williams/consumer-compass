84b9d657c32ebf9762f29ac9a2b27224eca16021
Nathan Williams <nate88williams@gmail.com>
2024-03-12 20:43:13 -0500

    Implement Outlet in App component
    Added the Outlet component to App.jsx for improved routing. The update uses react-router-dom to manage the application's routing and dynamically load components, enhancing overall navigation experience.

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 // import './App.css'
 import NavBar from "./Components/NavBar/NavBar.tsx";
 import Footer from "./Components/Footer/Footer.tsx";
+import { Outlet } from "react-router-dom";
 
 function App() {
   return (
     <div className="min-h-screen">
         <NavBar />
+        <Outlet />
         <Footer />
     </div>
   )
