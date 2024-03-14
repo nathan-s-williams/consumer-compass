2e4a15705dc34eb912b134a2b008f26adcbd0956
Nathan Williams <nate88williams@gmail.com>
2024-03-14 10:52:17 -0500

    Add image and improve typography in LoginPage
    The commit integrates an image into the LoginPage through the CardBody component and refines the typography by using the Roboto font and adjusting text sizes. It also ignores the unused vars rule with ESLint, preventing potential linting errors in the future.

--- src/Views/Login.tsx
+++ src/Views/Login.tsx
@@ -1,14 +1,27 @@
-import { Card, CardBody } from "@nextui-org/react";
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+import React from "react";
+import { Card, CardBody, Image } from "@nextui-org/react";
+import laptop from "../assets/analysis-laptop-business.jpg";
 
 function HomePage() {
     return (
         <>
+            <Card>
+                <CardBody>
+
+                </CardBody>
+                <Image
+                    removeWrapper
+                    alt="Dashboard on laptop screen"
+                    className="z-0 w-full h-full object-cover"
+                    src={ laptop }
+                />
+            </Card>
             <div className="h-screen">
-                <div>
-                    Easy
-                    Understandable
-                    Dashboards
-                    Market Research that Makes Sense
+                <div className="flex flex-col gap-3 font-roboto">
+                    <p className="text-2xl font-medium">Easy, Understandable</p>
+                    <p className="text-6xl font-medium">Dashboards</p>
+                    <p className="text-2xl font-medium">Market Research that Makes Sense</p>
                 </div>
                 <div className="w-2/5">
                     <h1 className="text-4xl">Navigate to Success</h1>
