185d2d3bca963a21f0854b19b99354e3c964a4ab
Nathan Williams <nate88williams@gmail.com>
2024-03-14 10:52:46 -0500

    Add React import and adjust style in Footer component
    Added React import statement to Footer.tsx that was previously missing. Also made changes to the existing div element's style, specifically removing the 'bg-gray-200' style. This makes the style of Footer component more consistent with the rest of the application.

--- src/Components/Footer/Footer.tsx
+++ src/Components/Footer/Footer.tsx
@@ -1,6 +1,9 @@
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+import React from "react";
+
 function Footer() {
     return(
-        <div className="fixed bottom-0 inset-x-0 h-20 bg-gray-200 flex items-center">
+        <div className="fixed bottom-0 inset-x-0 h-20 flex items-center">
             <p className="ml-5 text-black">Copyright { new Date().getFullYear() }</p>
         </div>
     );
