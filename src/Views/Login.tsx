9931c9e94f526c3082cdb712940ca0ca8321d21c
Nathan Williams <nate88williams@gmail.com>
2024-03-19 19:57:05 -0500

    Refactor Login page structure and styling
    Updated Login.tsx to incorporate CardHeader from "@nextui-org/react" library and amended Card structure and class styling. The changes include relocating and restyling of title "Dashboards" inside a Card component for better UI organization. Removed some redundant components and comments to simplify the page layout.

--- src/Views/Login.tsx
+++ src/Views/Login.tsx
@@ -1,37 +1,48 @@
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 import React from "react";
-import { Card, CardBody, Image } from "@nextui-org/react";
+import { Card, CardHeader, CardBody, Image } from "@nextui-org/react";
 import laptop from "../assets/analysis-laptop-business.jpg";
 
 function HomePage() {
     return (
-        <>
-            <Card>
-                <CardBody>
+        <div className="flex flex-col items-center">
+            <Card className="my-10 h-[700px] min-w-[1045px] max-w-[1300px] w-[90%]">
+                <CardHeader className="z-10">
 
-                </CardBody>
+                </CardHeader>
                 <Image
                     removeWrapper
                     alt="Dashboard on laptop screen"
                     className="z-0 w-full h-full object-cover"
-                    src={ laptop }
+                    src={laptop}
                 />
+            </Card>
+            {/*<div className="absolute left-[10%] top-32 flex flex-col min-w-[235px] font-roboto">*/}
+            {/*    <p className="text-[3vw] font-medium">*/}
+            {/*        Easy,<br/>*/}
+            {/*        Understandable*/}
+            {/*    </p>*/}
+            {/*    <p className="text-[5vw] font-medium">Dashboards</p>*/}
+            {/*</div>*/}
+            <Card className="absolute left-[10%] top-32 flex flex-col min-w-[235px] font-roboto">
+                <p className="text-[3vw] font-medium">
+                    Easy,<br/>
+                    Understandable
+                </p>
+                <p className="text-[5vw] font-medium">Dashboards</p>
             </Card>
             <div className="h-screen">
                 <div className="flex flex-col gap-3 font-roboto">
-                    <p className="text-2xl font-medium">Easy, Understandable</p>
-                    <p className="text-6xl font-medium">Dashboards</p>
                     <p className="text-2xl font-medium">Market Research that Makes Sense</p>
                 </div>
                 <div className="w-2/5">
                     <h1 className="text-4xl">Navigate to Success</h1>
-                    <i className="text-xl">by taking your business to the next level!</i><br /><br />
+                    <i className="text-xl">by taking your business to the next level!</i><br/><br/>
                     <p>
                         Our dynamic dashboards will help you gain the insight you need to understand
                         your customers. Simply upload your
                         sales data and start generating your personalized dashboard... your way. Pick and choose
                         the elements you want to see and reorganize them to get the perfect view of your data.
-                        
                     </p>
                     <p>
                         Feeling nervous about making decisions from complicated charts and graphs?
@@ -40,14 +51,7 @@ function HomePage() {
                     </p>
                 </div>
             </div>
-            {/*<div className="relative h-screen bg-light-soft-blue">*/}
-            {/*    <Card className="absolute left-1/4 bottom-[-70%] h-96 w-[700px]">*/}
-            {/*        <CardBody>*/}
-            {/*            <h1>Welcome to <i>Consumer Compass</i>!</h1>*/}
-            {/*        </CardBody>*/}
-            {/*    </Card>*/}
-            {/*</div>*/}
-        </>
+        </div>
     );
 }
 
