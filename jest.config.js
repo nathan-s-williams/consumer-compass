21f804f02c10460d7b91db767ed68ccf07d6d10f
Nathan Williams <nate88williams@gmail.com>
2024-03-22 21:35:07 -0500

    Remove Jest configuration file
    The jest.config.js file has been deleted from the root directory of the project. This file was previously defining the roots of the project, transform properties, testRegex, and moduleFileExtensions. The removal indicates a change in testing framework or configuration for this TypeScript project.

--- jest.config.js
+++
@@ -1,9 +0,0 @@
-// eslint-disable-next-line no-undef
-module.exports = {
-    roots: ['<rootDir>/src'],
-    transform: {
-        '^.+\\.tsx?$': 'ts-jest',
-    },
-    testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.tsx?$',
-    moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
-};
